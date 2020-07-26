329c5f8d8ca304292f1c0372447880254953d795
Nan Chen <divetothesea@gmail.com>
2020-07-26 09:37:22 +0800

    encapsu

---
+++ oop.js
@@ -0,0 +1,149 @@
+window.addEventListener('load', () => {
+  class Framer {
+    constructor() {
+      this.container = document.querySelector('.container');
+      this.upload = document.querySelector('#upload');
+      this.download = document.querySelector('#download');
+      this.inputUp = document.querySelector('#imageUpload');
+      this.init();
+      this.options = {
+        shadowOffsetX: 5,
+        shadowOffsetY: 5,
+        shadowBlur: 4,
+        shadowColor: 'rgba(0,0,0,0.3)',
+      };
+    }
+    init() {
+      this.inputUp.addEventListener('change', (e) => {
+        this.handleFiles(e.target.files);
+      });
+    }
+    handleFiles(files) {
+      for (let file of files) {
+        var imageType = /^image\//;
+
+        if (!imageType.test(file.type)) {
+          continue;
+        }
+        //console.log(files);
+        this.image = new Image();
+        this.image.file = file;
+
+        this.reader = new FileReader();
+
+        var p = new Promise((res, rej) => {
+          this.reader.addEventListener('load', (e) => {
+            this.image.src = e.target.result;
+            res();
+          });
+        });
+
+        p.then(() => {
+          // console.log(image.height);
+          if (this.image.height > 400) {
+            let ratio = this.image.width / this.image.height;
+            this.image.height = '400';
+            this.image.width = `${ratio * 400}`;
+          }
+
+          this.background = document.createElement('canvas');
+          document.querySelector('.container').appendChild(this.background);
+          this.draw(this.image, this.background);
+
+          document.querySelector('#add').addEventListener('click', () => {
+            this.addShadow(this.image, this.background, this.options);
+          });
+          document.querySelector('#remove').addEventListener('click', () => {
+            this.addShadow(this.image, this.background);
+          });
+        });
+
+        this.reader.readAsDataURL(file);
+      }
+
+      this.download.addEventListener('click', () => {
+        document.querySelectorAll('canvas').forEach((element) => {
+          let url = element.toDataURL('image/jpeg');
+          let a = document.createElement('a');
+          let event = new MouseEvent('click');
+          a.download = name || 'photo';
+          a.href = url;
+          a.dispatchEvent(event);
+        });
+      });
+    }
+    draw(img, element, options) {
+      var mar = 80;
+      img.width = (1, Math.floor(img.width));
+      img.height = Math.max(1, Math.floor(img.height));
+      var landscape = img.width > img.height ? true : false;
+
+      if (landscape) {
+        element.width = img.width + mar;
+        element.height = element.width;
+        var x = mar / 2;
+        var y = (element.height - img.height) / 2;
+      } else {
+        element.height = img.height + mar;
+        element.width = element.height;
+        var y = mar / 2;
+        var x = (element.width - img.width) / 2;
+      }
+
+      let ctx = element.getContext('2d');
+      ctx.clearRect(0, 0, element.width, element.height);
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+
+      const data = ctx.getImageData(0, 0, img.width, img.height).data;
+
+      this.color = this.getImageColor(img, data);
+      ctx.fillStyle = this.color;
+      ctx.fillRect(0, 0, element.width, element.width);
+
+      if (options) {
+        let keys = Object.keys(options);
+        keys.forEach((key) => {
+          ctx[key] = options[key];
+        });
+      }
+      ctx.fillStyle = `${this.color}`;
+
+      ctx.fillRect(x, y, img.width, img.height);
+      ctx.drawImage(img, x, y, img.width, img.height);
+    }
+    getImageColor(img, data) {
+      var r = 1,
+        g = 1,
+        b = 1;
+      for (var row = 0; row < img.height; row++) {
+        for (var col = 0; col < img.width; col++) {
+          if (row == 0) {
+            r += data[img.width * row + col];
+            g += data[img.width * row + col + 1];
+            b += data[img.width * row + col + 2];
+          } else {
+            r += data[(img.width * row + col) * 4];
+            g += data[(img.width * row + col) * 4 + 1];
+            b += data[(img.width * row + col) * 4 + 2];
+          }
+        }
+      }
+      r /= img.width * img.height;
+      g /= img.width * img.height;
+      b /= img.width * img.height;
+      r = Math.round(r);
+      g = Math.round(g);
+      b = Math.round(b);
+      let color = `rgb(${r},${g},${b})`;
+      //console.log(color);
+      return color;
+    }
+    addShadow(file, element, options) {
+      this.draw(file, element, options);
+    }
+    removeShadow(file) {
+      this.draw(file, element);
+    }
+  }
+  let f = new Framer();
+});
